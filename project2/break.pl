b43048525b575ae9f0dac9b84c0f5a2162d0f357
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-11-24 17:03:30 -0700

    preprocess

---
+++ project2/break.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+while (<STDIN>) {
+  chomp;
+  if (/^(.*?):(.*?)$/) {
+    $key=$1; $rec=$2;
+    # BDB treats backslash as a special character, and we would get rid of it
+    # (if still present)!
+    $rec =~ s/\\/&#92;/g;
+    print $key, "\n", $rec, "\n";
+  }
+}
+
---
+++ project2/preprocess.sh
@@ -0,0 +1,11 @@
+sort -o terms_sorted_10.txt terms_10.txt -u 
+sort -o years_sorted_10.txt years_10.txt -u 
+sort -o recs_sorted_10.txt recs_10.txt -u 
+    
+perl break.pl < terms_sorted_10.txt > terms_temp.txt
+perl break.pl < years_sorted_10.txt > years_temp.txt
+perl break.pl < recs_sorted_10.txt > recs_temp.txt
+    
+db_load -T -t btree -f terms_temp.txt te.idx
+db_load -T -t btree -f years_temp.txt ye.idx
+db_load -T -t hash -f recs_temp.txt re.idx
