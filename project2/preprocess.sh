a0ad414005fee7a9d21aee876775da8e69992140
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-11-24 20:16:33 -0700

    fix a bug

--- project2/preprocess.sh
+++ project2/preprocess.sh
@@ -6,6 +6,6 @@ perl break.pl < terms_sorted_10.txt > terms_temp.txt
 perl break.pl < years_sorted_10.txt > years_temp.txt
 perl break.pl < recs_sorted_10.txt > recs_temp.txt
     
-db_load -T -t btree -f terms_temp.txt te.idx
-db_load -T -t btree -f years_temp.txt ye.idx
-db_load -T -t hash -f recs_temp.txt re.idx
+db_load -c duplicates=1 -T -t btree -f terms_temp.txt te.idx
+db_load -c duplicates=1 -T -t btree -f years_temp.txt ye.idx
+db_load -c duplicates=1 -T -t hash -f recs_temp.txt re.idx
